f574099e77d17a42bb2fcd4b6097dda41a7f137f
jesusvalure <115124922+jesusvalure@users.noreply.github.com>
2025-10-21 21:31:48 -0600

    Crear proyecto en Netbeans

---
+++ Programa1MatriculaCalificaciones/src/main/java/com/mycompany/programa1matriculacalificaciones/Programa1MatriculaCalificaciones.java
@@ -0,0 +1,8 @@
+package com.mycompany.programa1matriculacalificaciones;
+
+public class Programa1MatriculaCalificaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
